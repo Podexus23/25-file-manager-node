b00fe340a5fa6bda37e23798868900d6ccbc957e
Podexus23 <santon23@gmail.com>
2024-10-08 23:41:27 +0300

    feat: add homedir check at the start

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 /**
  * 
-    
-    
     By default program should prompt user in console to print commands and wait for results
     In case of unknown operation or invalid input (missing mandatory arguments, wrong data in arguments, etc.) Invalid input message should be shown and user should be able to enter another command
     In case of error during execution of operation Operation failed message should be shown and user should be able to enter another command (e.g. attempt to perform an operation on a non-existent file or    work on a non-existent path should result in the operation fail)
     User can't go upper than root directory (e.g. on Windows it's current local drive root). If user tries to do so, current working directory doesn't change
  */
 
-/**
- *
- */
-
-import fs, { createReadStream, createWriteStream } from "node:fs";
+import { homedir } from "node:os";
 import { stdin } from "node:process";
-import path, { join } from "path";
 
 const userData = {
   userName: "Username",
+  currentDir: "",
 };
 
-function getName() {
+function setUserName() {
   const args = process.argv.slice(2);
   const usernameArg = "--username=";
   if (args.length < 1) {
@@ -39,43 +33,48 @@ function getName() {
   else userData.userName = name;
   return null;
 }
-
-function showHello() {
-  process.stdout.write(`Welcome to the File Manager, ${userData.userName}!\n`);
+function setUserDir() {
+  userData.currentDir = homedir();
 }
-function showGoodBye() {
-  process.stdout.write(
-    `Thank you for using File Manager, ${userData.userName}, goodbye!\n`
-  );
+
+function printHello(name) {
+  process.stdout.write(`Welcome to the File Manager, ${name}!\n`);
+}
+function printGoodBye(name) {
+  process.stdout.write(`Thank you for using File Manager, ${name}, goodbye!\n`);
+}
+function printWorkingDirectory(path) {
+  console.log(`You are currently in ${path}`);
 }
 
 function main() {
   // START PROGRAM
-  getName();
-  showHello();
+  setUserName();
+  printHello(userData.userName);
+  setUserDir();
+  printWorkingDirectory(userData.currentDir);
   /**
    * At the start of the program and after each end of input/operation current working directory should be printed in following way:
-    You are currently in path_to_working_directory
-    Starting working directory is current user's home directory (for example, on Windows it's something like system_drive/Users/Username) 
+    You are currently in path_to_working_directory\
    */
-  //MID PROGRAMM CYCLE
+  //MID PROGRAM CYCLE
   let inputData;
   //start working with cycle
-  process.stdin.setEncoding("utf-8");
-  process.stdin.on("data", (data) => {
+  stdin.setEncoding("utf-8");
+  stdin.on("data", (data) => {
     inputData = data.trim();
     console.log(`Hi, this is data: ${inputData}`);
     //check for exit
     if (inputData == ".exit") {
-      showGoodBye();
+      printGoodBye(userData.userName);
       process.exit(0);
     }
   });
   //----------------------------------------------------------------------
   //END of PROGRAM
   process.on("SIGINT", (signal) => {
-    showGoodBye();
-    process.exit();
+    printGoodBye(userData.userName);
+    process.exit(0);
   });
 }
 

044c74bd5f384b71783f9ae31cea1f73f7f22273
Podexus23 <santon23@gmail.com>
2024-10-09 15:27:07 +0300

    feat: add up comm for path

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 
 import os, { homedir } from "node:os";
 import { stdin } from "node:process";
+import path from "node:path";
 
 const userData = {
   userName: "Username",
@@ -57,10 +58,10 @@ function osOps(flag) {
     }
     case "--cpus": {
       let cpusData = os.cpus();
-      console.log(`Overall amount of logical CPUs: ${cpusData.length} GHz`);
+      console.log(`Overall amount of logical CPUs: ${cpusData.length}`);
       console.log("Models:");
       for (let cpu of cpusData) {
-        console.log(`${cpu.model.trim()}. Speed: ${cpu.speed / 1000}`);
+        console.log(`${cpu.model.trim()}. Speed: ${cpu.speed / 1000} GHz`);
       }
       break;
     }
@@ -83,6 +84,15 @@ function osOps(flag) {
     }
   }
 }
+// PATH OPERATIONS ------------------------------
+function pathUp() {
+  if (path.parse(userData.currentDir).root == userData.currentDir) {
+    console.log(`You've reached the root folder ${userData.currentDir}`);
+    return;
+  }
+  const newPath = path.dirname(userData.currentDir);
+  userData.currentDir = newPath;
+}
 
 function main() {
   // START PROGRAM
@@ -94,28 +104,38 @@ function main() {
    * At the start of the program and after each end of input/operation current working directory should be printed in following way:
     You are currently in path_to_working_directory\
    */
-  //! TEST CASES ------------------------------------
-  // osOps("--EOL");
-  // osOps("--cpus");
-  // osOps("--homedir");
-  // osOps("--username");
-  // osOps("--architecture");
-  // osOps("--archites");
+  //! TEST CASES ------------------
+  //os check
+  //  osOps("--EOL");
+  //  osOps("--cpus");
+  //  osOps("--homedir");
+  //  osOps("--username");
+  //  osOps("--architecture");
+  //  osOps("--archites");
+
   //!MID PROGRAM CYCLE
   let inputData;
   let exitFlag = 0;
-  //start working with cycle
+  //take data from terminal
   stdin.setEncoding("utf-8");
   stdin.on("data", (data) => {
-    inputData = data.trim();
-    console.log(`Hi, this is data: ${inputData}`);
+    inputData = data.toString().trim();
+    // console.log(`Hi, this is data: ${inputData}`);
+    const sortedInputData = inputData.split(" ");
+    // console.log(`sorted data ${inputData.split(" ")}`);
+    //check for os ops
+    if (sortedInputData[0] == "os") osOps(sortedInputData[1]);
+    if (sortedInputData[0] == "up") {
+      pathUp();
+      printWorkingDirectory(userData.currentDir);
+    }
     //check for exit
     if (inputData == ".exit") {
       printGoodBye(userData.userName);
       process.exit(0);
     }
   });
-  //!END of PROGRAM----------------------------------------------------------------------
+  //!EXIT FROM PROGRAM -------------------------------
   process.on("SIGINT", (signal) => {
     printGoodBye(userData.userName);
     process.exit(0);

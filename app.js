438c39deb2ebd372e652639e4226f6029af4eb01
Podexus23 <santon23@gmail.com>
2024-10-10 10:47:05 +0300

    refactor: removed path and ls ops to module files

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-/**
- *  By default program should prompt user in console to print commands and wait for results
-    In case of unknown operation or invalid input (missing mandatory arguments, wrong data in arguments, etc.) Invalid input message should be shown and user should be able to enter another command
-    In case of error during execution of operation Operation failed message should be shown and user should be able to enter another command (e.g. attempt to perform an operation on a non-existent file or    work on a non-existent path should result in the operation fail)
-    User can't go upper than root directory (e.g. on Windows it's current local drive root). If user tries to do so, current working directory doesn't change
- */
-
 import { homedir } from "node:os";
 import { stdin } from "node:process";
-import path from "node:path";
 import fs from "fs/promises";
 
 import { osOps } from "./modules/osOps.js";
@@ -16,8 +8,9 @@ import {
   printHello,
   printWorkingDirectory,
 } from "./modules/printOps.js";
+import { goToPath, pathUp, showCurrentLs } from "./modules/pathAndLsOps.js";
 
-const userData = {
+export const userData = {
   userName: "Username",
   currentDir: "",
 };
@@ -46,62 +39,30 @@ function setUserDir() {
   process.chdir(userData.currentDir);
 }
 
+async function commandsController(data) {
+  let inputData = data.toString().trim();
+  const sortedInputData = inputData.split(" ");
+  //check for operation
+  if (sortedInputData[0] == "os") osOps(sortedInputData[1]);
+  if (sortedInputData[0] == "up") {
+    pathUp();
+    printWorkingDirectory(userData.currentDir);
+  }
+  if (sortedInputData[0] == "cd") {
+    await goToPath(sortedInputData[1]);
+    printWorkingDirectory(userData.currentDir);
+  }
+  if (sortedInputData[0] == "ls") {
+    await showCurrentLs();
+    printWorkingDirectory(userData.currentDir);
+  }
+  //check for exit
+  if (inputData == ".exit" || sortedInputData[0] == ".exit") {
+    printGoodBye(userData.userName);
+    process.exit(0);
+  }
+}
 // PATH OPERATIONS ------------------------------
-function pathUp() {
-  if (path.parse(userData.currentDir).root == userData.currentDir) {
-    console.error(`You've reached the root folder ${userData.currentDir}`);
-    return;
-  }
-  const newPath = path.dirname(userData.currentDir);
-  userData.currentDir = newPath;
-  process.chdir(userData.currentDir);
-}
-/**
- * @param {string | undefined} newPath
- */
-
-async function goToPath(newPath) {
-  if (!newPath) {
-    console.error("cd: undefined or wrong path.");
-    return;
-  }
-  const absolutePath = path.resolve(newPath);
-
-  try {
-    await fs.access(absolutePath);
-    userData.currentDir = absolutePath;
-    process.chdir(userData.currentDir);
-  } catch (error) {
-    console.log(error.message);
-  }
-  console.log(absolutePath);
-}
-
-async function showCurrentLs() {
-  try {
-    const lsData = await fs.readdir(userData.currentDir, {
-      withFileTypes: true,
-    });
-    const showData = lsData
-      .map((e) => {
-        return {
-          ["Name"]: e.name,
-          ["Type"]: e.isDirectory() ? "directory" : e.isFile() ? "file" : null,
-        };
-      })
-      .filter((e) => e.Type != null);
-    const dir = showData
-      .filter((e) => e.Type == "directory")
-      .sort((a, b) => a - b);
-    const files = showData
-      .filter((e) => e.Type == "file")
-      .sort((a, b) => a - b);
-    const concatSortedData = [...dir, ...files];
-    console.table(concatSortedData);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
 
 function main() {
   // START PROGRAM
@@ -112,32 +73,10 @@ function main() {
   //! TEST CASES ------------------
 
   //!MID PROGRAM CYCLE
-  let inputData;
   //take data from terminal
   stdin.setEncoding("utf-8");
-  stdin.on("data", async (data) => {
-    inputData = data.toString().trim();
-    const sortedInputData = inputData.split(" ");
-    //check for operation
-    if (sortedInputData[0] == "os") osOps(sortedInputData[1]);
-    if (sortedInputData[0] == "up") {
-      pathUp();
-      printWorkingDirectory(userData.currentDir);
-    }
-    if (sortedInputData[0] == "cd") {
-      await goToPath(sortedInputData[1]);
-      printWorkingDirectory(userData.currentDir);
-    }
-    if (sortedInputData[0] == "ls") {
-      await showCurrentLs();
-      printWorkingDirectory(userData.currentDir);
-    }
-    //check for exit
-    if (inputData == ".exit" || sortedInputData[0] == ".exit") {
-      printGoodBye(userData.userName);
-      process.exit(0);
-    }
-  });
+  stdin.on("data", commandsController);
+
   //!EXIT FROM PROGRAM -------------------------------
   process.on("SIGINT", (signal) => {
     printGoodBye(userData.userName);
---
+++ modules/pathAndLsOps.js
@@ -0,0 +1,56 @@
+import path from "path";
+import fs from "fs/promises";
+import { userData } from "../app.js";
+
+export function pathUp() {
+  if (path.parse(userData.currentDir).root == userData.currentDir) {
+    console.error(`You've reached the root folder ${userData.currentDir}`);
+    return;
+  }
+  const newPath = path.dirname(userData.currentDir);
+  userData.currentDir = newPath;
+  process.chdir(userData.currentDir);
+}
+
+export async function goToPath(newPath) {
+  if (!newPath) {
+    console.error("cd: undefined or wrong path.");
+    return;
+  }
+  const absolutePath = path.resolve(newPath);
+
+  try {
+    await fs.access(absolutePath);
+    userData.currentDir = absolutePath;
+    process.chdir(userData.currentDir);
+  } catch (error) {
+    console.error(error.message);
+  }
+  console.log(absolutePath);
+}
+
+export async function showCurrentLs() {
+  try {
+    const lsData = await fs.readdir(userData.currentDir, {
+      withFileTypes: true,
+    });
+    const showData = lsData
+      .map((e) => {
+        return {
+          ["Name"]: e.name,
+          ["Type"]: e.isDirectory() ? "directory" : e.isFile() ? "file" : null,
+        };
+      })
+      .filter((e) => e.Type != null);
+    const dir = showData
+      .filter((e) => e.Type == "directory")
+      .sort((a, b) => a - b);
+    const files = showData
+      .filter((e) => e.Type == "file")
+      .sort((a, b) => a - b);
+    const concatSortedData = [...dir, ...files];
+    console.table(concatSortedData);
+  } catch (error) {
+    console.error(error.message);
+  }
+}

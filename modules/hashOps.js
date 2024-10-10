211b02431580624b516d73b9254799392507c65f
Podexus23 <santon23@gmail.com>
2024-10-10 11:58:25 +0300

    refactor: remade hash func from promise

--- modules/hashOps.js
+++ modules/hashOps.js
@@ -1,7 +1,5 @@
-import path, { resolve } from "path";
-import crypto, { createHash } from "crypto";
-import { log } from "console";
-import { on } from "events";
+import { resolve } from "path";
+import { createHash } from "crypto";
 import { createReadStream } from "fs";
 
 export async function hashOps(path) {
@@ -9,14 +7,14 @@ export async function hashOps(path) {
     console.error("hash: undefined or wrong path.");
     return;
   }
-  try {
-    const absolutePath = resolve(path);
+  const absolutePath = resolve(path);
+  let prom = new Promise((resolve, reject) => {
     const rs = createReadStream(absolutePath);
     const hash = createHash("sha256");
-    rs.on("error", (err) => console.error(err.message));
+
+    rs.on("error", (err) => reject(err));
     rs.on("data", (chunk) => hash.update(chunk));
-    rs.on("end", () => console.log(hash.digest("hex")));
-  } catch (error) {
-    console.error(error.message);
-  }
+    rs.on("end", () => resolve(hash.digest("hex")));
+  }).catch((err) => console.error(err.message));
+  return await prom;
 }

a9269d027cb4f8f969559e41a069553c4f43f1c4
Podexus23 <santon23@gmail.com>
2024-10-10 12:13:17 +0300

    refactor: take data from obj, not from args

--- modules/printOps.js
+++ modules/printOps.js
@@ -1,3 +1,5 @@
+import { userData } from "../app.js";
+
 export function printHello(name) {
   process.stdout.write(`Welcome to the File Manager, ${name}!\n`);
 }
@@ -5,5 +7,8 @@ export function printGoodBye(name) {
   process.stdout.write(`Thank you for using File Manager, ${name}, goodbye!\n`);
 }
 export function printWorkingDirectory(path) {
-  console.log("\x1b[36m%s\x1b[0m", `You are currently in ${path}`);
+  console.log(
+    "\x1b[36m%s\x1b[0m",
+    `You are currently in ${userData.currentDir}`
+  );
 }
